5e9569a8017af89fb9009fef50fca6c625ff0071
moqa19 <64950524+moqa19@users.noreply.github.com>
2022-11-17 13:02:45 -0700

    a

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,21 +5,46 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
 
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    
+    CarMake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=100)
+    year = models.DateField()
 
+    SEDAN = 'Sedan'
+    WAGON = 'WAGON'
+    SUV = 'SUV'
+
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (WAGON, 'WAGON '),
+        (SUV, 'SUV')
+    ]
+    Type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Car Make Name: " + self.CarMake.name + \
+               "Car Make Description: " + self.CarMake.description + \
+               "Type: " + self.Type + \
+               "Year: " + str(self.year.year) + \
+               "Dealer ID: " + str(self.dealer_id) 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
